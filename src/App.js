b9206d9189157035ec2a63b28f47c80e86c1dc85
kasunsiyambalapitiya <kasun.siyambalapitiya@gmail.com>
2020-11-29 22:01:29 +0530

    Add start test and build images

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and create amazing web pages
         </a>
       </header>
     </div>
